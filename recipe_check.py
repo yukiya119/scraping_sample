e8eed7c1f73d50e9042c82344398f687820872f6
yukiya119 <yukinosuke911@gmail.com>
2022-01-30 12:07:15 +0900

    ほぼ完成かな？

--- recipe_check.py
+++ recipe_check.py
@@ -1,31 +1,41 @@
 # noinspection PyUnresolvedReferences
-import time
-
-# Seleniumの中のwebdriverをインポート
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 URL = "https://cookpad.com"  # グローバル変数
 
 
+def chromedriver_options():
+    # オプション設定
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # ヘッドレスモード
+    # options.add_argument("--blink-settings=imagesEnabled=false")  # 画像無効
+    # options.add_argument("--enable-javascript")  # JS無効
+    return options
+
+
 def search_by_food(driver, food):
     driver.get(f"{URL}")
     driver.implicitly_wait(10)
 
-    driver.find_element(By.ID, "keyword").send_keys(food)  # 検索にfoodの内容を入力すべし
-    driver.find_element(By.ID, "submit_button").click()  # 検索ボタンを押すべし
-
-    time.sleep(10)
+    driver.find_element(By.ID, "keyword").send_keys(food)  # 検索にfoodの内容を入力
+    driver.find_element(By.ID, "submit_button").click()  # 検索ボタンを押す
+    # time.sleep(10)
 
 
 def get_recipes(driver):
     recipe_previews = driver.find_elements(By.CLASS_NAME, "recipe-preview")
     # print(recipe_previews)
 
+    recipes = []
     for recipe_preview in recipe_previews:
         recipe_title = recipe_preview.find_element(By.CLASS_NAME, "recipe-title").text
         recipe_url = recipe_preview.find_element(By.CLASS_NAME, "recipe-title").get_attribute('href')
-        print(recipe_title, recipe_url)
+        recipes.append({
+            "title": recipe_title,
+            "url": recipe_url})
+    return recipes
 
 
 def main():
@@ -37,10 +47,13 @@ def main():
     # driver.implicitly_wait(10)
     # driver.close()
 
-    # withの方法(ドライバーclose忘れ防止)
-    with webdriver.Chrome() as driver:
+    # withの方法(close忘れ防止)
+    with webdriver.Chrome(options=chromedriver_options()) as driver:
         search_by_food(driver, food)
-        get_recipes(driver)
+        recipes = get_recipes(driver)
+
+        for recipe in recipes:
+            print(f"レシピ名 {recipe['title']}, URL:{recipe['url']}")
 
 
 if __name__ == '__main__':
